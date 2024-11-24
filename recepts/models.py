1ca7a9e38c6572a39b00aa641ccd68179c6427f6
saymios <daniojenezis@gmail.com>
2024-11-24 10:47:15 +0200

    add base recipe model

--- recepts/models.py
+++ recepts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=205, verbose_name='Назва рецепту')
+    body = models.CharField(max_length=3000, verbose_name='Рецепт')
+    date_taken = models.DateTimeField(auto_now_add=True)
+    
